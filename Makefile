5439d6b50308cd9ff3ea7649dd49cb8d71b55a09
Lutts Cao <lutts.cao@gmail.com>
2015-07-13 17:21:48 +0800

    initial commit

---
+++ Makefile
@@ -0,0 +1,21 @@
+all: build
+
+build: FORCE
+	make -C build
+	cp build/compile_commands.json ./
+
+prepare:
+	-mkdir build
+	cd build && cmake -DCMAKE_EXPORT_COMPILE_COMMANDS=ON ..
+
+run-%: FORCE
+	cd build && ctest -R $*
+
+Makefile:;
+
+%:: FORCE
+	make -C build $@
+
+FORCE:
+
+.PHONY: FORCE all
---
+++ sdi2/sdi_zh_CN.ts
@@ -0,0 +1,240 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="zh_CN">
+<context>
+    <name>SdiWindow</name>
+    <message>
+        <location filename="sdiwindow.cpp" line="50"/>
+        <location filename="sdiwindow.cpp" line="251"/>
+        <location filename="sdiwindow.cpp" line="270"/>
+        <source>%1[*] - %2</source>
+        <translation>%1[*]-%2</translation>
+    </message>
+    <message>
+        <location filename="sdiwindow.cpp" line="50"/>
+        <source>unnamed</source>
+        <translation>unnamed</translation>
+    </message>
+    <message>
+        <location filename="sdiwindow.cpp" line="50"/>
+        <location filename="sdiwindow.cpp" line="187"/>
+        <location filename="sdiwindow.cpp" line="242"/>
+        <location filename="sdiwindow.cpp" line="251"/>
+        <location filename="sdiwindow.cpp" line="261"/>
+        <location filename="sdiwindow.cpp" line="270"/>
+        <source>SDI</source>
+        <translation>SDI</translation>
+    </message>
+    <message>
+        <location filename="sdiwindow.cpp" line="60"/>
+        <source>Done</source>
+        <translation>完成</translation>
+    </message>
+    <message>
+        <location filename="sdiwindow.cpp" line="78"/>
+        <source>About SDI</source>
+        <translation>关于SDI</translation>
+    </message>
+    <message>
+        <location filename="sdiwindow.cpp" line="78"/>
+        <source>A single document interface application.</source>
+        <translation>一个SDI应用</translation>
+    </message>
+    <message>
+        <location filename="sdiwindow.cpp" line="83"/>
+        <source>&amp;New</source>
+        <translation>新建(&amp;N)</translation>
+    </message>
+    <message>
+        <location filename="sdiwindow.cpp" line="84"/>
+        <source>Ctrl+N</source>
+        <translation>Ctrl+N</translation>
+    </message>
+    <message>
+        <location filename="sdiwindow.cpp" line="85"/>
+        <source>Create a new document</source>
+        <translation>创建新文档</translation>
+    </message>
+    <message>
+        <location filename="sdiwindow.cpp" line="88"/>
+        <source>&amp;Open</source>
+        <translation>打开(&amp;O)</translation>
+    </message>
+    <message>
+        <location filename="sdiwindow.cpp" line="89"/>
+        <source>Ctrl+O</source>
+        <translation>Ctrl+O</translation>
+    </message>
+    <message>
+        <location filename="sdiwindow.cpp" line="90"/>
+        <source>Open a document</source>
+        <translation>打开文档</translation>
+    </message>
+    <message>
+        <location filename="sdiwindow.cpp" line="93"/>
+        <source>&amp;Save</source>
+        <translation>保存(&amp;S)</translation>
+    </message>
+    <message>
+        <location filename="sdiwindow.cpp" line="94"/>
+        <source>Ctrl+S</source>
+        <translation>Ctrl+S</translation>
+    </message>
+    <message>
+        <location filename="sdiwindow.cpp" line="95"/>
+        <source>Save the document</source>
+        <translation>保存文档</translation>
+    </message>
+    <message>
+        <location filename="sdiwindow.cpp" line="98"/>
+        <source>Save &amp;As</source>
+        <translation>另存为(&amp;A)</translation>
+    </message>
+    <message>
+        <location filename="sdiwindow.cpp" line="99"/>
+        <source>Save the document as</source>
+        <translation>文档另存为</translation>
+    </message>
+    <message>
+        <location filename="sdiwindow.cpp" line="102"/>
+        <source>&amp;Close</source>
+        <translation>关闭(&amp;C)</translation>
+    </message>
+    <message>
+        <location filename="sdiwindow.cpp" line="103"/>
+        <source>Ctrl+W</source>
+        <translation>Ctrl+W</translation>
+    </message>
+    <message>
+        <location filename="sdiwindow.cpp" line="104"/>
+        <source>Close this document</source>
+        <translation>关闭文档</translation>
+    </message>
+    <message>
+        <location filename="sdiwindow.cpp" line="107"/>
+        <source>E&amp;xit</source>
+        <translation>退出(&amp;x)</translation>
+    </message>
+    <message>
+        <location filename="sdiwindow.cpp" line="108"/>
+        <source>Ctrl+Q</source>
+        <translation>Ctrl+Q</translation>
+    </message>
+    <message>
+        <location filename="sdiwindow.cpp" line="109"/>
+        <source>Quit the application</source>
+        <translation>退出应用</translation>
+    </message>
+    <message>
+        <location filename="sdiwindow.cpp" line="113"/>
+        <source>Cu&amp;t</source>
+        <translation>剪切(&amp;t)</translation>
+    </message>
+    <message>
+        <location filename="sdiwindow.cpp" line="114"/>
+        <source>Ctrl+X</source>
+        <translation>Ctrl+X</translation>
+    </message>
+    <message>
+        <location filename="sdiwindow.cpp" line="115"/>
+        <source>Cut</source>
+        <translation>剪切</translation>
+    </message>
+    <message>
+        <location filename="sdiwindow.cpp" line="120"/>
+        <source>&amp;Copy</source>
+        <translation>复制(&amp;C)</translation>
+    </message>
+    <message>
+        <location filename="sdiwindow.cpp" line="121"/>
+        <source>Ctrl+C</source>
+        <translation>Ctrl+C</translation>
+    </message>
+    <message>
+        <location filename="sdiwindow.cpp" line="122"/>
+        <source>Copy</source>
+        <translation>复制</translation>
+    </message>
+    <message>
+        <location filename="sdiwindow.cpp" line="128"/>
+        <source>&amp;Paste</source>
+        <translation>粘贴(&amp;P)</translation>
+    </message>
+    <message>
+        <location filename="sdiwindow.cpp" line="129"/>
+        <source>Ctrl+V</source>
+        <translation>Ctrl+V</translation>
+    </message>
+    <message>
+        <location filename="sdiwindow.cpp" line="130"/>
+        <source>Paste</source>
+        <translation>粘贴</translation>
+    </message>
+    <message>
+        <location filename="sdiwindow.cpp" line="134"/>
+        <source>&amp;About</source>
+        <translation>关于(&amp;A)</translation>
+    </message>
+    <message>
+        <location filename="sdiwindow.cpp" line="135"/>
+        <source>About this application</source>
+        <translation>关于应用程序</translation>
+    </message>
+    <message>
+        <location filename="sdiwindow.cpp" line="138"/>
+        <source>About &amp;Qt</source>
+        <translation>关于&amp;Qt</translation>
+    </message>
+    <message>
+        <location filename="sdiwindow.cpp" line="139"/>
+        <source>About the Qt toolkit</source>
+        <translation>关于QT toolkit</translation>
+    </message>
+    <message>
+        <location filename="sdiwindow.cpp" line="147"/>
+        <source>&amp;File</source>
+        <translation>文件(&amp;F)</translation>
+    </message>
+    <message>
+        <location filename="sdiwindow.cpp" line="158"/>
+        <source>&amp;Edit</source>
+        <translation>编译(&amp;E)</translation>
+    </message>
+    <message>
+        <location filename="sdiwindow.cpp" line="163"/>
+        <source>&amp;Help</source>
+        <translation>帮助(&amp;H)</translation>
+    </message>
+    <message>
+        <location filename="sdiwindow.cpp" line="172"/>
+        <source>File</source>
+        <translation>文件</translation>
+    </message>
+    <message>
+        <location filename="sdiwindow.cpp" line="175"/>
+        <source>Edit</source>
+        <translation>编辑</translation>
+    </message>
+    <message>
+        <location filename="sdiwindow.cpp" line="188"/>
+        <source>The document has unsaved changes.
+Do you want to save it before it is closed?</source>
+        <translation>文档有未保存的修改</translation>
+    </message>
+    <message>
+        <location filename="sdiwindow.cpp" line="230"/>
+        <source>Save As</source>
+        <translation>另存为</translation>
+    </message>
+    <message>
+        <location filename="sdiwindow.cpp" line="242"/>
+        <source>Failed to save file.</source>
+        <translation>保存文件失败</translation>
+    </message>
+    <message>
+        <location filename="sdiwindow.cpp" line="261"/>
+        <source>Failed to open file.</source>
+        <translation>打开文件失败</translation>
+    </message>
+</context>
+</TS>
